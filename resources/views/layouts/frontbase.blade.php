65d4ddb2ee870ab3d275276b11863f6c9870a027
furkanndemirr <furkannndemirrr53@gmail.com>
2022-04-03 23:08:44 +0300

    Web Template Implementation

--- resources/views/layouts/frontbase.blade.php
+++ resources/views/layouts/frontbase.blade.php
@@ -1,22 +1,68 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-    <title>App Name - @yield('title')</title>
-    @yield('head')
+
+    <!-- META ============================================= -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
+    <meta name="robots" content="" />
+
+    <!-- DESCRIPTION -->
+    <meta name="description" content="EduChamp : Education HTML Template" />
+
+    <!-- OG -->
+    <meta property="og:title" content="EduChamp : Education HTML Template" />
+    <meta property="og:description" content="EduChamp : Education HTML Template" />
+    <meta property="og:image" content="" />
+    <meta name="format-detection" content="telephone=no">
+
+    <!-- FAVICONS ICON ============================================= -->
+    <link rel="icon" href="{{asset('assets')}}/images/favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets')}}/images/favicon.png" />
+
+    <!-- PAGE TITLE HERE ============================================= -->
+    <title>@yield("title")</title>
+
+    <!-- MOBILE SPECIFIC ============================================= -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--[if lt IE 9]>
+    <script src="{{asset('assets')}}/js/html5shiv.min.js"></script>
+    <script src="{{asset('assets')}}/js/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- All PLUGINS CSS ============================================= -->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets')}}/assets.css">
+
+    <!-- TYPOGRAPHY ============================================= -->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets')}}/typography.css">
+
+    <!-- SHORTCODES ============================================= -->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets')}}/css/shortcodes/shortcodes.css">
+
+    <!-- STYLESHEETS ============================================= -->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets')}}/css/style.css">
+    <link class="skin" rel="stylesheet" type="text/css" href="{{asset('assets')}}/css/color/color-1.css">
+
+    <!-- REVOLUTION SLIDER CSS ============================================= -->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets')}}/vendors/revolution/css/layers.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets')}}/vendors/revolution/css/settings.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets')}}/vendors/revolution/css/navigation.css">
+    <!-- REVOLUTION SLIDER END -->
+    @yield("head")
 </head>
+<body id="bg">
 
-<body>
-<h1>Header</h1>
-
-@section('sidebar')
-    This is the master sidebar.
-@show
+@include("home.header")
 
 
-<div class="container">
-    @yield('content')
-</div>
+@yield('content')
 
-<h1>Footer</h1>
+@include("home.footer")
 @yield('foot')
+
 </body>
 </html>
