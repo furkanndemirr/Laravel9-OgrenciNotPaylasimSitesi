d87fb5ebc652e791185be4301413fff77525b742
furkanndemirr <furkannndemirrr53@gmail.com>
2022-04-10 20:44:06 +0300

    Category Update Data to Database

---
+++ resources/views/admin/category/edit.blade.php
@@ -0,0 +1,95 @@
+@extends('layouts.adminbase')
+@section('title' ,'Edit Category : '.$data->title)
+
+@section('content')
+    <section role="main" class="content-body">
+        <header class="page-header">
+            <h2>Edit Category : {{$data->title}}</h2>
+
+            <div class="right-wrapper pull-right">
+                <ol class="breadcrumbs">
+                    <li>
+                        <a href="/admin">
+                            <i class="fa fa-home"></i>
+                        </a>
+                    </li>
+                    <li><span>Pages</span></li>
+                    <li><span>Edit Category</span></li>
+                </ol>
+
+                <a class="sidebar-right-toggle" data-open="sidebar-right"><i class="fa fa-chevron-left"></i></a>
+            </div>
+        </header>
+        <!-- start: page -->
+
+        <section class="panel">
+            <header class="panel-heading">
+                <div class="panel-actions">
+                    <a href="#" class="fa fa-caret-down"></a>
+                    <a href="#" class="fa fa-times"></a>
+                </div>
+
+                <h2 class="panel-title">Category Elements</h2>
+            </header>
+            <div class="panel-body">
+                <form class="form-horizontal form-bordered" method="post" action="/admin/category/update/{{$data->id}}">
+                    @csrf
+                    <div class="form-group">
+                        <label class="col-md-3 control-label" for="inputDefault">Title</label>
+                        <div class="col-md-6">
+                            <input type="text" class="form-control" name="title" value="{{$data->title}}">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label" for="inputDefault">Keywords</label>
+                        <div class="col-md-6">
+                            <input type="text" class="form-control" name="keywords" value="{{$data->keywords}}">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label" for="inputDefault">Description</label>
+                        <div class="col-md-6">
+                            <input type="text" class="form-control" name="description" value="{{$data->description}}">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-md-3 control-label" for="inputDefault">Image</label>
+
+                        <div  class="col-md-3">
+                            <input type="file" name="image" value="{{$data->image}}">
+
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-md-3 control-label" for="inputDefault">Status</label>
+                        <div class="col-md-6">
+                            <select name="status">
+                                <option selected>{{$data->status}}</option>
+                            <option>True</option>
+                            <option>False</option>
+                            </select>
+                        </div>
+
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-md-5 control-label" for="inputDefault"></label>
+
+                        <div class="col-md-6">
+                            <input type="submit"  value="Update Data">
+                        </div>
+                    </div>
+
+
+                </form>
+            </div>
+        </section>
+
+
+        <!-- end: page -->
+
+
+    </section>
+@endsection
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -9,7 +9,7 @@
             <div class="right-wrapper pull-right">
                 <ol class="breadcrumbs">
                     <li>
-                        <a href="index.html">
+                        <a href="/admin">
                             <i class="fa fa-home"></i>
                         </a>
                     </li>
