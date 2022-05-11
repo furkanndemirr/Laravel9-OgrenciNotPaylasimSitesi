1a97b521b32824c47e35c829268592e5395297e2
furkanndemirr <furkannndemirrr53@gmail.com>
2022-05-11 12:04:21 +0300

    Home Page Dynamic Slider

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Content;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -9,7 +10,11 @@ class HomeController extends Controller
     //
     public function index()
     {
-        return view('home.index');
+        $sliderdata=Content::limit(4)->get();
+        return view('home.index',[
+            'sliderdata'=>$sliderdata
+            ]
+        );
     }
     public function show()
     {
--- resources/views/home/careusel.blade.php
+++ resources/views/home/careusel.blade.php
@@ -3,25 +3,28 @@
     <div id="header-carousel" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img class="w-100" src="{{asset('assets')}}/img/carousel-1.jpg" alt="Image">
+                <img class="w-100" src="{{asset('assets')}}/images/anaslider.jpg" alt="Image" style="width: 1000px;height: 350px">
                 <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                     <div class="p-3" style="max-width: 800px;">
                         <h4 class="text-primary text-uppercase font-weight-normal mb-md-3">Creative Interior Design</h4>
-                        <h3 class="display-3 text-white mb-md-4">Make Your Home Better</h3>
+                        <h3 class="display-3 text-white mb-md-4">NOTLAR</h3>
                         <a href="" class="btn btn-primary py-md-3 px-md-5 mt-2 mt-md-4">Learn More</a>
                     </div>
                 </div>
             </div>
-            <div class="carousel-item">
-                <img class="w-100" src="{{asset('assets')}}/img/carousel-2.jpg" alt="Image">
+
+            @foreach($sliderdata as $rs)
+            <div class="carousel-item ">
+                <img class="w-100" src="{{Storage::url($rs->image)}}" alt="Image" style="width: 1000px;height: 350px">
                 <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                     <div class="p-3" style="max-width: 800px;">
                         <h4 class="text-primary text-uppercase font-weight-normal mb-md-3">Creative Interior Design</h4>
-                        <h3 class="display-3 text-white mb-md-4">Stay At Home In Peace</h3>
+                        <h3 class="display-3 text-white mb-md-4">{{$rs->title}}</h3>
                         <a href="" class="btn btn-primary py-md-3 px-md-5 mt-2 mt-md-4">Learn More</a>
                     </div>
                 </div>
             </div>
+            @endforeach
         </div>
         <a class="carousel-control-prev" href="#header-carousel" data-slide="prev">
             <div class="btn btn-primary" style="width: 45px; height: 45px;">
